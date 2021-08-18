b935d41bc0432fc439d18c22719ef4982fb4020c
Steve Xing <stevethesteve990@gmail.com>
2021-08-17 22:15:57 -0400

    Calculated Amount of Repeating Sections needed

--- src/app/background/background.component.ts
+++ src/app/background/background.component.ts
@@ -8,11 +8,13 @@ import { Component, OnInit } from '@angular/core';
 export class BackgroundComponent implements OnInit {
 
 	observer;
-	numRepeat;
+	ratio;
 	heightsMap;
+	numRepeat;
 
 	constructor() {
 		this.heightsMap = new Map<string, number>();
+		this.ratio=0;
 		this.numRepeat=0;
 
 		// THE FUNCTION BELOW WILL RUN WHENEVER CHANGE IS DETECTED
@@ -30,13 +32,22 @@ export class BackgroundComponent implements OnInit {
 				totalBackgroundHeights += background[1];
 			}
 			
-			contentHeight = this.heightsMap.get('content') as number;
+			contentHeight = this.heightsMap.get('content') as number + 800; // adding on 800 because ResizeObserver might *not be recording down margins
 			repeatingBackgroundHeight = this.heightsMap.get('testRepeatingBg') as number;
-			
 			// content is definitely less than or equal to background height total, but add an ABS to be sure
-			this.numRepeat = Math.abs(totalBackgroundHeights - contentHeight) / repeatingBackgroundHeight;
-			console.log(this.numRepeat);
+			this.ratio = Math.abs(totalBackgroundHeights - contentHeight) / repeatingBackgroundHeight; // hence why its named "ratio"
+			
+			// ratio = 15.5 and up, you're safe to not add any sections in
+			if(this.ratio < 15.5){ // otherwise, start adding them in
+				let ratioTrimmed:number = 15.5 - this.ratio; // trimming it down for convenience sake
 
+				// every 0.2, add a new section in
+				this.numRepeat = Math.ceil(ratioTrimmed / 0.2);
+
+			}
+
+			// console.log(this.numRepeat); our FINAL result
+			
 		});
 
 		// THE FUNCTION ABOVE WILL RUN WHENEVER CHANGE IS DETECTED
